621654936b46cd4f707aef610e41c53385ba9727
JMES-64 <jeanmuribar1999@hotmail.com>
2025-05-25 14:46:42 -0600

    Creación del proyecto
    Se crea el proyecto a utilizar en ADO.NET

---
+++ LogIn_ADO.NET_Version/LogIn_ADO.NET_Version/Models/competencia.cs
@@ -0,0 +1,14 @@
+﻿namespace LogIn_ADO.NET_Version.Models
+{
+    public class competencia
+    {
+        int IDC { get; set; }
+        //ID de la competencia
+        string Nombre_C { get; set; }
+        //Nombre de la competencia
+        int Competencia { get; set; }
+        //Nivel de competencia, va del 1 al 5
+        int IDU { get; set; }
+        //ID del usuario al que la competencia está ligada
+    }
+}
---
+++ LogIn_ADO.NET_Version/LogIn_ADO.NET_Version/Models/usser.cs
@@ -0,0 +1,19 @@
+﻿namespace LogIn_ADO.NET_Version.Models
+{
+    public class usser
+    {
+        int IDU { get; set; }
+        //ID del usuario
+        string Nombre { get; set; }
+        //Nombre del usuario
+        string Correo { get; set; }
+        //Correo del usuario
+        string Pass { get; set; }
+        //Contraseña del usuario
+        int Adm { get; set; }
+        //Administrador del sistema, debe ser 0 o 1
+        int Own { get; set; }
+        //Propietario del sistema, debe ser 0 o 1
+
+    }
+}
